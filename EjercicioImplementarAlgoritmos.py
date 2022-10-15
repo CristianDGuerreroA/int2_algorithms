ccc1fbfe3488d83a36e8d17770031b3087322e5a
JuanJosePazUC <81938588+JuanJosePazUC@users.noreply.github.com>
2022-10-15 14:41:41 -0500

    agregar algoritmos funcionales regresion lineal y pca

--- EjercicioImplementarAlgoritmos.py
+++ EjercicioImplementarAlgoritmos.py
@@ -13,6 +13,10 @@ import scipy.cluster.hierarchy as sch
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
+plt.rcParams['figure.figsize'] = (16, 9)
+plt.style.use('ggplot')
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 
 def display_gui(top, data_csv):
@@ -301,9 +305,7 @@ def rf_indicator(data_csv):
 
 
 
-
-def lineFunc(x,slope,intercept):
-    return slope * x + intercept    
+   
 
 def rl_indicator(data_csv):
     if traverse_matrix_boolean_string(data_csv) == True:
@@ -311,15 +313,16 @@ def rl_indicator(data_csv):
     else:
         features = data_csv["age"]
         labels = data_csv["speed"]
-        slope, intercept, r, p, std_err = stats.linregress(features, labels)
+        slope_, intercept_, r, p, std_err = stats.linregress(features, labels)
 
-        lineY = list(map(lineFunc, features))
+        lineY = list(map(lambda x : slope_ * x + intercept_, features))
         print (lineY)
 
         plt.scatter(features, labels)
         plt.plot(features, lineY)
         plt.show()
 
+ 
 
 def Dendograma(dataset):
 
@@ -337,9 +340,50 @@ def den_indicator(data_csv):
         Dendograma(data_csv)
         messagebox.showinfo("", "Que se dice desde el Dendograma")
 
+def AlgoritmoPCA(dataframe):
+    #normalizamos los datos
+    scaler=StandardScaler()
+    df = dataframe.drop(['comprar'], axis=1) # quito la variable dependiente "Y"
+    scaler.fit(df) # calculo la media para poder hacer la transformacion
+    X_scaled=scaler.transform(df)# Ahora si, escalo los datos y los normalizo
+
+    #Instanciamos objeto PCA y aplicamos
+    pca=PCA(n_components=9) # Otra opción es instanciar pca sólo con dimensiones nuevas hasta obtener un mínimo "explicado" ej.: pca=PCA(.85)
+    pca.fit(X_scaled) # obtener los componentes principales
+    X_pca=pca.transform(X_scaled) # convertimos nuestros datos con las nuevas dimensiones de PCA
+
+    #Vemos que con 5 componentes tenemos algo mas del 85% de varianza explicada
+
+    #graficamos el acumulado de varianza explicada en las nuevas dimensiones
+    plt.plot(np.cumsum(pca.explained_variance_ratio_))
+    plt.xlabel('number of components')
+    plt.ylabel('cumulative explained variance')
+    plt.show()
+
+    #graficamos en 2 Dimensiones, tomando los 2 primeros componentes principales
+    Xax=X_pca[:,0]
+    Yax=X_pca[:,1]
+    labels=dataframe['comprar'].values
+    cdict={0:'red',1:'green'}
+    labl={0:'Alquilar',1:'Comprar'}
+    marker={0:'*',1:'o'}
+    alpha={0:.3, 1:.5}
+    fig,ax=plt.subplots(figsize=(7,5))
+    fig.patch.set_facecolor('white')
+    for l in np.unique(labels):
+        ix=np.where(labels==l)
+        ax.scatter(Xax[ix],Yax[ix],c=cdict[l],label=labl[l],s=40,marker=marker[l],alpha=alpha[l])
+
+    plt.xlabel("First Principal Component",fontsize=14)
+    plt.ylabel("Second Principal Component",fontsize=14)
+    plt.legend()
+    plt.show()
+
 def pca_indicator(data_csv):
     if traverse_matrix_boolean_string(data_csv) == True:
         messagebox.showerror("Error - File", "No se pueden cargar datos String en el DataSet para PCA")
+    else:
+        AlgoritmoPCA(data_csv)
 
 if __name__ == "__main__":
     route_csv = NULL
