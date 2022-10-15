00774e3a0db70336c41f64d72b6e59733130da2a
JuanJosePazUC <81938588+JuanJosePazUC@users.noreply.github.com>
2022-10-15 12:49:24 -0500

    Algoritmo PCA
    Se agregaron los metodos correspondientes para el algoritmo PCA

--- EjercicioImplementarAlgoritmos.py
+++ EjercicioImplementarAlgoritmos.py
@@ -159,6 +159,10 @@ import scipy.cluster.hierarchy as sch
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
+plt.rcParams['figure.figsize'] = (16, 9)
+plt.style.use('ggplot')
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 
 def lineFunc(x,slope,intercept):
@@ -196,9 +200,51 @@ def den_indicator(data_csv):
         Dendograma(data_csv)
         messagebox.showinfo("", "Que se dice desde el Dendograma")
 
+def AlgoritmoPCA(dataframe):
+    #normalizamos los datos
+    scaler=StandardScaler()
+    df = dataframe.drop(['comprar'], axis=1) # quito la variable dependiente "Y"
+    scaler.fit(df) # calculo la media para poder hacer la transformacion
+    X_scaled=scaler.transform(df)# Ahora si, escalo los datos y los normalizo
+
+    #Instanciamos objeto PCA y aplicamos
+    pca=PCA(n_components=9) # Otra opción es instanciar pca sólo con dimensiones nuevas hasta obtener un mínimo "explicado" ej.: pca=PCA(.85)
+    pca.fit(X_scaled) # obtener los componentes principales
+    X_pca=pca.transform(X_scaled) # convertimos nuestros datos con las nuevas dimensiones de PCA
+
+    #Vemos que con 5 componentes tenemos algo mas del 85% de varianza explicada
+
+    #graficamos el acumulado de varianza explicada en las nuevas dimensiones
+    plt.plot(np.cumsum(pca.explained_variance_ratio_))
+    plt.xlabel('number of components')
+    plt.ylabel('cumulative explained variance')
+    plt.show()
+
+    #graficamos en 2 Dimensiones, tomando los 2 primeros componentes principales
+    Xax=X_pca[:,0]
+    Yax=X_pca[:,1]
+    labels=dataframe['comprar'].values
+    cdict={0:'red',1:'green'}
+    labl={0:'Alquilar',1:'Comprar'}
+    marker={0:'*',1:'o'}
+    alpha={0:.3, 1:.5}
+    fig,ax=plt.subplots(figsize=(7,5))
+    fig.patch.set_facecolor('white')
+    for l in np.unique(labels):
+        ix=np.where(labels==l)
+        ax.scatter(Xax[ix],Yax[ix],c=cdict[l],label=labl[l],s=40,marker=marker[l],alpha=alpha[l])
+
+    plt.xlabel("First Principal Component",fontsize=14)
+    plt.ylabel("Second Principal Component",fontsize=14)
+    plt.legend()
+    plt.show()
+
+
 def pca_indicator(data_csv):
     if traverse_matrix_boolean_string(data_csv) == True:
         messagebox.showerror("Error - File", "No se pueden cargar datos String en el DataSet para PCA")
+    else:
+        AlgoritmoPCA(data_csv)
 
 if __name__ == "__main__":
     route_csv = NULL
